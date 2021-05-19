ba9513e59954f5166108b24749fb7d9ff4f8e992
krishpranav <krisna.pranav@gmail.com>
2021-05-19 16:12:53 +0530

    [+] Env Error Specifications [+]

---
+++ spec/insta_crawler/errors/env_error_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe InstagramCrawler::Errors::EnvError do
+    subject(:env_error) { described_class.new }
+    it '#initialize' do
+      error_message = env_error.to_s
+      expect(error_message).to eq("Undefined env variable sessionid\nYou should setting env variable 'export sessionid=[your instagram sessionid]' and execute again.")
+    end
+  end
+  
