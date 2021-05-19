34f156dfe5f1e4289cc9dc2d81b7079636c0af01
krishpranav <krisna.pranav@gmail.com>
2021-05-19 16:07:50 +0530

    [+] Spec Helper [+]

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require "bundler/setup"
+require "insta_crawler"
+require 'coveralls'
+Coveralls.wear!
+
+RSpec.configure do |config|
+    config.example_status_persistence_file_path = ".rspec_status"
+
+    config.disable_monkey_patching!
+
+    config.expect_with :rspec do |c|
+        c.syntax = :expect
+    end
+end
