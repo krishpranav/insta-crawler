cdb495cad4766fa0f14c31f3fa3d9b4077cfbdc6
krishpranav <krisna.pranav@gmail.com>
2021-05-19 16:32:21 +0530

    [+] Base Functions [+]

---
+++ lib/insta_crawler/parser/base.rb
@@ -0,0 +1,33 @@
+module InstaCrawler
+    module Parser
+        class Base
+            private
+
+            def parse_post(posts, time)
+                posts.each.with_index(1) do |post, index|
+                    url =
+                        post["node"]["is_video"]?
+                        post["node"]["video_url"] :
+                        post["node"]["display_url"]
+                    output(time, url)
+                    File.download(url, "post/#{time}", "#{index}.#{time}")
+                end
+            end
+
+            def output(time, url)
+                Logger.info "[#{time}]".light_cyan + " #{url}"
+            end
+
+            def parse_to_date(ts)
+                Time.at(ts).strftime('%Y-%m-%dT%H:%M')
+            end
+
+            def check_after_time(time)
+                if Config.after_date && (Config.parse_after_date > time)
+                    Logger.info "\nSuccess, the files after #{Config.after_date} have been downloaded!".light_green
+                    exit
+                end
+            end
+        end
+    end
+end
